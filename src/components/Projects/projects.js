ed8da3af5e3b06df9efa47247f293babdd23537a
szewah <swgrleong@gmail.com>
2020-09-15 12:25:47 -0400

    Changed projects to class

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,10 +1,9 @@
 import React, {Component}from 'react';
 //material UI
-import {Grid, Box, Link, GridList, GridListTile} from '@material-ui/core';
+import {Grid, GridListTile, GridList, Slide} from '@material-ui/core';
 import './style.css';
-import { makeStyles} from '@material-ui/core/styles';
+import { withStyles} from '@material-ui/core/styles';
 import tileData from './tileData'
-
 
 
 
@@ -15,51 +14,82 @@ const divStyle = {
 }
 
 
-const useStyles = makeStyles({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        flexGrow: 1,
-        justifyContent: 'center',
-        overflow: 'hidden',
-        marginTop: 20,
-        paddingBottom: 20,
+const overlay = {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: '100%',
+    width: '100',
+    opacity: 0,
+    transition: '.5s ease',
+    backgroundColor: '#008CBA'
+  }
+
+
+const useStyles = theme => ({
+    a: {
+        display: 'block',
+        height: '100%',
+      },
+    img: {
+        objectFit: 'none',
+        width: '100%',
+        height: '100%',
     },
-    gridList: {
-    }
 })
 
 
 
-export default function Projects(){
+class Projects extends Component {
 
-    const changeBackground = (e) => {
+    state = {
+        isHovering: false,
+    }
+    
+    changeBackground = (e) => {
         e.target.style.opacity = '0.5';
+        console.log('changed')
+      };
 
-      }
-
-    const normalBaackground =(e) => {
+    normalBackground = (e) => {
         e.target.style.opacity = '1';
-    }
+    };
+    
 
-    const preventDefault = (event) => {
-        console.log(event);
-    }
 
-    const classes = useStyles();
-    return (
-        <div style={divStyle}>
-            <Grid container justify='center'>
-                <h2 style={{fontWeight: 300, paddingTop: '50px'}}>PROJECTS</h2>
-            </Grid>
-            <GridList cellHeight={160} className={classes.gridList} cols={3}>
-                {tileData.map((tile) => (
-                    <GridListTile key={tile.img} cols={tile.cols || 1}>
-                    <img src={tile.img} alt={tile.title} />
-                    </GridListTile>
-                ))}
-            </GridList>
-        </div>
+    render() {
+        const classes = withStyles();
+        return (
+            <div style={divStyle}>
+                <Grid container justify='center'>
+                    <h2 style={{fontWeight: 300, paddingTop: '50px'}}>PROJECTS</h2>
+                </Grid>
+                {/* <Slide direction="up" mountOnEnter> */}
+                <GridList cellHeight={200} cols={4} 
+                    style={{marginLeft: '200px', marginRight: '200px', paddingTop: '20px', paddingBottom: '20px'}}>
+                    {tileData.map((tile) => (
+
+                        <GridListTile key={tile.id} cols={tile.cols || 1}>
+                            <a className={classes.a} href={`${tile.link}`} target='_blank' rel="noopener noreferrer" >
+                                <div className='overlay' style={overlay}></div>
+                                <img 
+                                    src={tile.img} 
+                                    alt={tile.title} 
+                                    className={classes.img} 
+                                    onMouseMove={this.changeBackground} 
+                                    onMouseLeave={this.normalBackground}
+                                />
+                            </a>
+                        </GridListTile>
+                    ))}
+                </GridList>
+                {/* </Slide> */}
+            </div>
         )
-}
+    }
+};
+
+export default withStyles(useStyles)(Projects);
 
--- src/components/Projects/tileData.js
+++ src/components/Projects/tileData.js
@@ -30,6 +30,36 @@ const tileData = [
       id: 4,
       link: 'https://github.com/szewah/friend_finder'
     },
+    {
+      img: '/static/images/retail.jpeg',
+      title: 'Popshop',
+      cols: 2,
+      rows: 3,
+      id: 5,
+      link: 'https://www.popshop.com/'
+    },
+    {
+      img: '/static/images/inventory.jpeg',
+      title: 'Bamazon',
+      rows: 3,
+      id: 6,
+      link: 'https://github.com/szewah/bamazon'
+    },
+    {
+      img: '/static/images/game.jpg',
+      title: 'Hangman',
+      rows: 3,
+      id: 7,
+      link: 'https://github.com/szewah/word-guess-game'
+    },
+    {
+      img: '/static/images/friends.jpg',
+      title: 'friends',
+      cols: 2,
+      rows: 3,
+      id: 8,
+      link: 'https://github.com/szewah/friend_finder'
+    },
   ];
   
   export default tileData;
